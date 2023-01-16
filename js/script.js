19519b684eeb304299f6ac9b27fbf4dd11b56282
Oksana Bashlachova <ookb2019@gmail.com>
2023-01-16 11:42:50 +0200

    Add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -25,8 +25,10 @@ for(let activeArticle of activeArticles){
 const articleSelector = clickedElement.getAttribute('href');
 console.log(articleSelector);
   /* find the correct article using the selector (value of 'href' attribute) */
-
+const targetArticle = document.querySelector(articleSelector);
+console.log(targetArticle)
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
